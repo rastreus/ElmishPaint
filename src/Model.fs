ff5b1058840b1cd04ca9a7771ea7cc84d30c1326
Russell Dillin <rastreus@gmail.com>
2026-02-07 17:37:16 -0800

    feat(model): add top-level editor model

---
+++ src/Model.fs
@@ -0,0 +1,12 @@
+namespace App
+
+type Model =
+    { Canvas: BitCanvas
+      Tool: Tool
+      ToolOptions: ToolOptions
+      Pattern: Pattern
+      Mouse: MouseState
+      Selection: Selection option
+      History: HistoryState
+      UI: UIState
+      ImportPreview: ImportPreview option }
