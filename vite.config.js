1a375198081eee18aa706938d6cf164c532dbaca
Russell Dillin <rastreus@gmail.com>
2026-02-08 11:46:34 -0800

    build(vite): remove vite plugin fable and transpile explicitly

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fable from 'vite-plugin-fable'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [fable({ fsproj: './src/ElmishPaint.fsproj', jsx: 'automatic' }), react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   root: "./src",
   build: {
     outDir: "../dist",
