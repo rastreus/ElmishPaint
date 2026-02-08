ed87796c59ab4f38101f750a9b977152c1081c7b
Russell Dillin <rastreus@gmail.com>
2026-02-07 22:58:49 -0800

    test(app): verify selecttool dispatch rerenders view

--- src/App.test.fs
+++ src/App.test.fs
@@ -17,4 +17,22 @@ Vitest.describe (
                 Vitest.expect(title).toHaveClass ("text-4xl")
             }
         )
+
+        Vitest.test (
+            "dispatching SelectTool updates rendered active tool",
+            fun () -> promise {
+                let ele = RTL.render (App.Components.App())
+                let activeTool = ele.getByTestId ("active-tool")
+                let selectLine = ele.getByTestId ("select-line")
+
+                Vitest.expect(activeTool).toHaveTextContent ("Active tool: Pencil")
+
+                do!
+                    RTL.act (fun () -> promise {
+                        RTL.fireEvent.click (selectLine)
+                    })
+
+                Vitest.expect(activeTool).toHaveTextContent ("Active tool: Line")
+            }
+        )
 )
