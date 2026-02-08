0cee0027cae0078ea262b7dc8d3bea0947ee64c1
Russell Dillin <rastreus@gmail.com>
2026-02-07 19:29:52 -0800

    feat(bitcanvas): implement mutation and clone operations

--- src/Canvas/BitCanvas.fs
+++ src/Canvas/BitCanvas.fs
@@ -35,3 +35,26 @@ module BitCanvas =
         else
             let byteIndex, bitMask = pixelLocation x y
             if (canvas.Data[byteIndex] &&& bitMask) <> 0uy then Black else White
+
+    let setPixel x y bit (canvas: App.BitCanvas) =
+        if inBounds x y then
+            let byteIndex, bitMask = pixelLocation x y
+            let current = canvas.Data[byteIndex]
+
+            canvas.Data[byteIndex] <-
+                match bit with
+                | Black -> current ||| bitMask
+                | White -> current &&& (byte (~~~(int bitMask)))
+
+    let fill bit (canvas: App.BitCanvas) =
+        let fillValue = if bit = Black then 255uy else 0uy
+        canvas.Data.fill(fillValue) |> ignore
+
+    let clear canvas = fill White canvas
+
+    let clone (canvas: App.BitCanvas) : App.BitCanvas =
+        {
+            Width = canvas.Width
+            Height = canvas.Height
+            Data = Constructors.Uint8Array.Create(canvas.Data)
+        }
