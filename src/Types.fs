d2b93d79c6852dbd3f67f0f6d8764c8e9ba10d81
Russell Dillin <rastreus@gmail.com>
2026-02-07 17:35:42 -0800

    feat(types): add editor state helper types

--- src/Types.fs
+++ src/Types.fs
@@ -23,3 +23,61 @@ type Modifiers =
       Ctrl: bool
       Alt: bool
       Meta: bool }
+
+type Point =
+    { X: int
+      Y: int }
+
+type BitCanvas =
+    { Width: int
+      Height: int
+      Data: byte array }
+
+type BrushSize =
+    | Brush1
+    | Brush2
+    | Brush4
+    | Brush8
+
+type RectangleMode =
+    | Outline
+    | Filled
+
+type ToolOptions =
+    { EraserBrushSize: BrushSize
+      RectangleMode: RectangleMode }
+
+type MouseState =
+    { IsDown: bool
+      Start: Point option
+      Last: Point option
+      Current: Point option
+      Modifiers: Modifiers }
+
+type Selection =
+    { BoundsStart: Point
+      BoundsEnd: Point
+      FloatingPixels: BitCanvas option
+      Offset: Point }
+
+type HistoryState =
+    { UndoStack: BitCanvas list
+      RedoStack: BitCanvas list
+      MaxDepth: int }
+
+type UIState =
+    { Zoom: int
+      Scroll: Point
+      HoveredPixel: Point option
+      IsBezelMode: bool }
+
+type ImportPreview =
+    { FileName: string
+      ThresholdOffset: int
+      Brightness: int
+      PreviewCanvas: BitCanvas }
+
+type ExportScale =
+    | Scale1x
+    | Scale2x
+    | Scale4x
